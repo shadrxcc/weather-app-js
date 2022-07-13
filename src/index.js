5e5cf824621503f6b84fcd91a99877273e41e09d
shadrach akaade <akaadesm@gmail.com>
2022-07-13 21:47:45 +0800

    fetch icon from api and modify styling

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import { getWeather } from './weather';
 const input = document.getElementById('search-bar');
 const button = document.getElementById('btn');
 
-
+//event listeners for search bar and button
 button.addEventListener('click', () => getWeather(input.value))
-
-;
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    getWeather(input.value);
+  }
+});
--- src/key.js
+++
@@ -1,3 +0,0 @@
-export const appId = () => {
-    appKey: a01278173aeb0079dafd8ea6bf28bfb1;
-}
